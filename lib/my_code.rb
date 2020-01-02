b82a85de572120b4a2249e1afdb189ade741d682
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-01-02 21:56:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
-# Your Code Here
+def map(arr)
+  newArr = []
+  i = 0 
+  
+  while i < arr.size do
+    newArr[i] = yield(arr[i])   
+    i+=1
+  end
+  newArr
+end
+
+def reduce(arr, start_opt = nil)
+  if start_opt  # If the optional starting point is true, make the return array equal to that
+    return_arr = start_opt
+    i = 0
+  else  # if no starting point, then start from 0
+    return_arr = arr[0]
+    i = 1
+  end
+  
+  while i < arr.length
+    return_arr = yield(return_arr, arr[i])
+    i += 1
+  end
+  return_arr
+  
+end
