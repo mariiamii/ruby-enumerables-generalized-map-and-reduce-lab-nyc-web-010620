0451e4bd83d855eacf1fb1179546ce2c6c77b01d
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-01-02 21:58:01 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
-# Your Code Here
+def map(arr)
+  newArr = []
+  i = 0 
+  
+  while i < arr.size do
+    newArr[i] = yield(arr[i])   
+    i += 1
+  end
+  newArr
+end
+
+def reduce(arr, start_opt = nil)
+  if start_opt  
+    return_arr = start_opt
+    i = 0
+  else  
+    return_arr = arr[0]
+    i = 1
+  end
+  
+  while i < arr.length
+    return_arr = yield(return_arr, arr[i])
+    i += 1
+  end
+  return_arr
+end
